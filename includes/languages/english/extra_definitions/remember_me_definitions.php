d9a0ad0be4d2eeab19da70d60def6eef922bb9b5
lat9 <lat9@vinosdefrutastropicales.com>
2025-02-09 10:39:44 -0500

    #34: Convert language defines to zc158+ array format

--- includes/languages/english/extra_definitions/remember_me_definitions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// -----
-// Part of the "Remember Me" plugin, modified for operation under Zen Cart v1.5.0 and later
-// by Cindy Merkin (aka lat9) of Vinos de Frutas Tropicales (vinosdefrutastropicales.com).
-//
-// Copyright (C) 2014-2020.  Vinos de Frutas Tropicales
-//
-define('TEXT_REMEMBER_ME', 'Remember me?');
-define('TEXT_REMEMBER_ME_ALT', 'Tick this box to be automatically logged in on your next visit. Note: This will place create a cookie in your current browser that can only be read by this website.');
